b82e79b2571b2b7cbf31d4e7a6fed69872ac3551
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-28 18:41:44 -0400

    working on user-input

---
+++ script.py
@@ -0,0 +1,35 @@
+import random
+
+# generate random letter between A-Z
+#random_letter =random.randbytes(A,Z)
+random_letter = str(random_letter)
+# Ask the user to supply one guess (i.e. letter) per round
+guess_count = 1
+# Let the user choose a level of difficulty at the beginning of the program.
+user_input = input("Pick a level")
+game_level = ("Easy", "Normal", "Hard")
+
+if game_level = "Easy"
+    print ("Only 4-6 characters")
+
+if game_level = "Normal"
+    print ("Only 6-8 characters")
+
+if game_level = "Hard"
+    print ("8 or more characters")
+
+    
+# the game asking the user if the user wants to quit or not
+while user_input != 'No' and game_ends == False
+# if the player uses all of the 8 tries incorrectly, they will lose.
+    if guess_count == 8;
+        print ("You Lost")
+        user_input = input('Would you like to try agian? Yes or No')
+        game_ends = True
+# if the player just quits
+        if user_intput == ]
+
+# Let the user choose a level of difficulty at the beginning of the program.
+#  let the user know how many letters the computer's word contains. 
+
+# f a user enters more than one letter, tell them the input is invalid and let them try again
