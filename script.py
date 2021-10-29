8c2724fc1a211623c06c69fc5882e3f2a19edd54
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-29 19:30:26 -0400

    rearranging items

--- script.py
+++ script.py
@@ -1,70 +1,143 @@
 import random 
+words = []
+underscores = []
+guesses =[]
 
-# generate random letter between A-Z
-#random_letter =random.randbytes(A,Z)
-random_letter = str(random_letter)
-
-guess_count = 1
-# Let the user choose a level of difficulty at the beginning of the program.
-user_input = input("Pick a level")
+user_input = input("Pick a level: Easy, Normal or Hard?")
 game_level = ("Easy", "Normal", "Hard")
 
-# At the start of the game, let the user know how many letters the computer's word contains.
 
-if game_level = "Easy"
+if game_level = "Easy":
     print ("This game only contains 4-6 characters")
     user_input = input("Continue" or "Go Back")
     
-    if user_input = ("Continue")
+    if user_input = Continue":
     print ("Start Game")
+        if user_input = ("Guess a Letter")
+
     
-    if user_input = ("Go Back")
+    if user_input = "Go Back":
     print ("Pick a level")
     game_level = input("Easy", "Normal", "Hard")
 
 
-if game_level = "Normal"
-    print ("This game only contains 6-8 characters")
-    user_input = input("Continue" or "Go Back")
+# if game_level = "Normal"
+#     print ("This game only contains 6-8 characters")
+#     user_input = input("Continue" or "Go Back")
     
-    if user_input = ("Continue")
-    print ("Start Game")
+#     if user_input = ("Continue")
+#     print ("Start Game")
+#     user_input = ("Guess a Letter")
 
-    if user_input = ("Go Back")
-    print ("Pick a level")
-    game_level = input("Easy", "Normal", "Hard")
+#     if user_input = ("Go Back")
+#     print ("Pick a level")
+#     game_level = input("Easy", "Normal", "Hard")
 
 
-if game_level = "Hard"
-    print ("This game contains 8 or more characters")
-    user_input = input("Continue" or "Go Back")
+# if game_level = "Hard"
+#     print ("This game contains 8 or more characters")
+#     user_input = input("Continue" or "Go Back")
     
-    if user_input = ("Continue")
-    print ("Start Game")
+#     if user_input = ("Continue")
+#     print ("Start Game")
+#     user_input = ("Guess a Letter")
 
-    if user_input = ("Go Back")
-    print ("Pick a level")
-    game_level = input("Easy", "Normal", "Hard")
+#     if user_input = ("Go Back")
+#     print ("Pick a level")
+#     game_level = input("Easy", "Normal", "Hard")
+
+# # to get the words from the text file 
+# with open ('words.txt') as file:
+#     strings = file.readlines()
+# # converting the words to strings
+#     for string in strings:
+#         words.append(string)
+#     random_word = words [7].lower()
+# # adds an underscore to each letter of the word and asks user to "guess a letter"
+#     word_length = range(len(random_word))
+#     for num in word_length:
+#         underscores.append('_')
+#     underscores = "".join(underscores)
+    
+#     print(underscores)
+#     user_input = input('Guess A Letter').lower()
+# # list comprehensions
+#     for index in range(len(random_word)):
+#         if random_word[index] == user_input:
+#             underscores = underscores[0:index] + user_input + underscores[index+1:]
+#     print('underscores', underscores)
+        
+        # random_word_list = [index+1:]
+    
+# generate random letter between A-Z
+#random_letter =random.randbytes(A,Z)
+# random_letter = str(random_letter)
+
+# guess_count = 1
+# # Let the user choose a level of difficulty at the beginning of the program. BEFORE THE GAME STARTS
+# user_input = input("Pick a level")
+# game_level = ("Easy", "Normal", "Hard")
+
+# # At the start of the game, let the user know how many letters the computer's word contains.
+
+# if game_level = "Easy"
+#     print ("This game only contains 4-6 characters")
+#     user_input = input("Continue" or "Go Back")
+    
+#     if user_input = ("Continue")
+#     print ("Start Game")
+#         if user_input = ("Guess a Letter")
+
+    
+#     if user_input = ("Go Back")
+#     print ("Pick a level")
+#     game_level = input("Easy", "Normal", "Hard")
+
+
+# if game_level = "Normal"
+#     print ("This game only contains 6-8 characters")
+#     user_input = input("Continue" or "Go Back")
+    
+#     if user_input = ("Continue")
+#     print ("Start Game")
+#     user_input = ("Guess a Letter")
+
+#     if user_input = ("Go Back")
+#     print ("Pick a level")
+#     game_level = input("Easy", "Normal", "Hard")
+
+
+# if game_level = "Hard"
+#     print ("This game contains 8 or more characters")
+#     user_input = input("Continue" or "Go Back")
+    
+#     if user_input = ("Continue")
+#     print ("Start Game")
+#     user_input = ("Guess a Letter")
+
+#     if user_input = ("Go Back")
+#     print ("Pick a level")
+#     game_level = input("Easy", "Normal", "Hard")
 
 
 
-# # Ask the user to supply one guess (i.e. letter) per roundThis letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
+# # # Ask the user to supply one guess (i.e. letter) per roundThis letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
 
 
-# the game asking the user if the user wants to quit or not //!CONDITIONS (YOU WON, YOU LOST, YOU QUIT, START OVER!//
-while user_input != 'No' and game_ends == False
-# (YOU LOST) if the player uses all of the 8 tries incorrectly, they will lose. *Game Over*
-    if guess_count == 8;
-        print ("You Lost")
-        user_input = input('Would you like to start over? Yes or No')
-        game_ends = True
-# (START OVER) if the player want to play again, game will start over and user gains 8 more tries
-        if user_input == ("Yes")
-            guess_count = 8
-            game_ends = False
+# # the game asking the user if the user wants to quit or not //!CONDITIONS (YOU WON, YOU LOST, YOU QUIT, START OVER!//
+# while user_input != 'No' and game_ends == False
+# # (YOU LOST) if the player uses all of the 8 tries incorrectly, they will lose. *Game Over*
+#     if guess_count == 8;
+#         print ("You Lost")
+#         user_input = input('Would you like to start over? Yes or No')
+#         game_ends = True
+# # (START OVER) if the player want to play again, game will start over and user gains 8 more tries
+#         if user_input == ("Yes")
+#             guess_count = 8
+#             game_ends = False
 
 
-# Let the user choose a level of difficulty at the beginning of the program.
-#  let the user know how many letters the computer's word contains. 
+# # Let the user choose a level of difficulty at the beginning of the program.
+# #  let the user know how many letters the computer's word contains. 
 
-# f a user enters more than one letter, tell them the input is invalid and let them try again
+# # f a user enters more than one letter, tell them the input is invalid and let them try again
